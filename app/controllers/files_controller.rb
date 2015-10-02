368b34096bb61f3ac3941b722d636bab435617c8
Meredith <bitcababy@gmail.com>
2015-10-02 09:18:38 -0400

    changed old.westonmath.org to files.westonmath.org

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,8 +1,8 @@
 class FilesController < ApplicationController
-	REDIRECT_URL = 'http://old.westonmath.org'
+	REDIRECT_URL = 'http://files.westonmath.org'
 
 	def pass_on
 		redirect_to "#{REDIRECT_URL}#{request.path}"
   end
-  
+
 end
