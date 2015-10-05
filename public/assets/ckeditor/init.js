a50f25f7a058ec0eb24dd69a4a7d33133434ee2b
Meredith <bitcababy@gmail.com>
2015-10-05 16:52:49 -0400

    Revert "precompiled assets again"
    This reverts commit 00cd81c2b59843228e9d3e7a43a3da25eab8ef15.

--- public/assets/ckeditor/init.js
+++ public/assets/ckeditor/init.js
@@ -1,4 +1,4 @@
-(function(){("undefined"==typeof window.CKEDITOR_BASEPATH||null===window.CKEDITOR_BASEPATH)&&(window.CKEDITOR_BASEPATH="http://files.westonmath.org/assets/assets/ckeditor/")}).call(this),/*
+(function(){("undefined"==typeof window.CKEDITOR_BASEPATH||null===window.CKEDITOR_BASEPATH)&&(window.CKEDITOR_BASEPATH="/assets/assets/ckeditor/")}).call(this),/*
 Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
