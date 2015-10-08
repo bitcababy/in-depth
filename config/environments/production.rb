a9564e46864dda2a2b57b47ca06c1b6661b600f3
Meredith <bitcababy@gmail.com>
2015-10-08 11:44:25 -0400

    Removed an extraneous slash from assets prefix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ InDepth::Application.configure do
 
   # Send deprecation notices to registered listeners
   # config.active_support.deprecation = :silence
-  config.assets.prefix = "/production/assets"
+  config.assets.prefix = "production/assets"
   config.assets.debug = true # Turn off after file browser works
 
 end
